a3e9e4d8f5c0145c623df55d983c7d634001df7f
Nikita Leev <nikita.leev@gmail.com>
2024-10-11 14:16:47 +0700

    improve test structure

--- test/MemeCoinTest.ts
+++ test/MemeCoinTest.ts
@@ -2,19 +2,32 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("MyToken", function () {
-  it("Should deploy the contract and check the contract address is not null", async function () {
-    // Get the contract factory
-    const MyToken = await ethers.getContractFactory("MemeCoin");
+  let myToken: any;
+  let owner: any;
 
-    // Deploy the contract with name, symbol, and initial supply
-    const myToken = await MyToken.deploy("NewAwesomeMeme", "NAM");
+  // Common logic to deploy the contract
+  const deployToken = async (name: string, symbol: string) => {
+    const contractFactory = await ethers.getContractFactory("MemeCoin");
+    const deployedContract = await contractFactory.deploy(name, symbol);
+    return deployedContract;
+  };
 
-    // Check that the contract address is valid (not null or zero address)
+  beforeEach(async () => {
+    // Get signers and deploy a new instance of the contract before each test
+    [owner] = await ethers.getSigners();
+    myToken = await deployToken("NewAwesomeMeme", "NAM");
+  });
+
+ it("Should deploy the contract and check the contract address is not null", async function () {
     expect(await myToken.getAddress()).to.not.be.null;
     expect(await myToken.name()).to.be.eq("NewAwesomeMeme");
     expect(await myToken.symbol()).to.be.eq("NAM");
 
-     // Console log the contract address
-     console.log("Contract deployed to:", await myToken.getAddress(), await myToken.name());
+    // Console log the contract address and token name
+    console.log("Contract deployed to:", await myToken.getAddress(), await myToken.name());
+  });
+
+  it("Should check that the owner balance is initially 0", async function () {
+    expect(await myToken.balanceOf(owner.address)).to.be.eq(0);
   });
 });
